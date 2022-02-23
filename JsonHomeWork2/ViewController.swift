8af31c41a2ff7692e592257efb3c833bdb50022f
osm0nov1 <94544948+osm0nov1@users.noreply.github.com>
2022-02-23 14:51:25 +0600

    Final

--- JsonHomeWork2/ViewController.swift
+++ JsonHomeWork2/ViewController.swift
@@ -6,12 +6,39 @@
 //
 
 import UIKit
+import Foundation
+
+struct First: Decodable {
+    let menu: Menu
+}
+
+struct Menu: Decodable {
+    let id: String
+    let value: String
+    let popup: Popup
+}
+struct Popup: Decodable {
+    let menuitem: [Menuitem]
+}
+struct Menuitem: Decodable {
+    let value: String
+    let onclick: String
+}
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+  
+        guard let asset = NSDataAsset(name: "json", bundle: Bundle.main) else {return}
+        
+        let model = try! JSONDecoder().decode(First.self, from: asset.data)
+        
+        dump(model)
+        
+        
+    
     }
 
 
